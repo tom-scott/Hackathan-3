e648f0d10f823b4bb7b56dcf2a6e340e9ccdf090
Tom Scott <thomas.scott@asos.com>
2017-07-13 15:01:42 +0100

    Added pass through

--- Program.fs
+++ Program.fs
@@ -7,24 +7,25 @@ open FSharp.Data
 type H = FSharp.Data.Http
 type J = FSharp.Data.JsonValue
 
-[<EntryPoint>]
-let main argv = 
-
 (*
-  choose
-    [ GET >=> choose
-        [ path "/hello" >=> OK "Hello GET"
-          path "/goodbye" >=> OK "Good bye GET" ]
-      POST >=> choose
-        [ path "/hello" >=> OK "Hello POST"
-          path "/goodbye" >=> OK "Good bye POST" ] ]
-
+Known issues:
+Only supporting one user at the moment
+Multiple quantities (what happens if bag expires but user then adds same item again... we only have 1 item!)
 
 *)
 
-// /commerce/bag/v3/bags/6c49a40f-9483-475b-970c-6aef41878544/product?expand=total,summary,address,spendLevelDiscount,deliveryoptions&language=en-GB
+module DataStore =
+  
+  let mutable private data = Map.empty<int,J>
 
-//pathScan "/commerce/bag/v3/bags/%s/product?expand=total,summary,address,spendLevelDiscount,deliveryoptions&language=en-GB" (fun bagId -> OK (sprintf "THis is bag %s" bag))
+  let update (variantId:int) (json:J) =
+    data <- data.Add(variantId, json)
+
+[<EntryPoint>]
+let main argv = 
+
+  let withStatusCode s status: WebPart =
+    fun ctx -> { ctx with response = { ctx.response with status = status; content = Bytes s }} |> succeed
 
   let bagApiUrl = "https://api.asos.com/commerce/bag/v3/bags/"
 
@@ -35,11 +36,27 @@ let main argv =
       "connection"
     |] |> Set.ofArray
 
+  let getHeaders (ctx:HttpContext) = 
+    ctx.request.headers
+    |> Seq.filter (fun (k,_) -> headersNotToPassOn.Contains (k.ToLower()) = false)
+    |> Seq.toList
+
+  let getQueryString (ctx:HttpContext) =
+    ctx.request.query
+    |> Seq.choose (fun (k,v) ->
+    match v with
+    | Some x -> Some (k, x)
+    | None -> None)
+    |> Seq.toList
+
   let (|RecordContainingKey|_|) (key:string) (json:J) =
     match json with
     | J.Record _ as record ->
       record.TryGetProperty key
     | _ -> None
+
+  //let replaceItems (j:J) =
+    
 
   let addToBag =
     POST
@@ -48,15 +65,10 @@ let main argv =
         
         let realUri = sprintf "%s%s/product" bagApiUrl bagId
         
-        let headers =
-          ctx.request.headers
-          |> Seq.filter (fun (k,_) -> headersNotToPassOn.Contains (k.ToLower()) = false)
-          |> Seq.toList
-          
-        let body = ctx.request
+        let headers = getHeaders ctx
+        let query = getQueryString ctx
 
         let requestJson = System.Text.Encoding.UTF8.GetString(ctx.request.rawForm)
-
         let json = J.Parse requestJson
 
         let variantId =
@@ -64,15 +76,8 @@ let main argv =
           | RecordContainingKey "variantId" (J.Number variantId) -> int variantId
           | _ -> failwith "Unexpected input"
 
-        let query =
-          ctx.request.query
-          |> Seq.choose (fun (k,v) ->
-            match v with
-            | Some x -> Some (k, x)
-            | None -> None)
-          |> Seq.toList
 
-        let! result = H.AsyncRequestString(realUri, query=query, headers=headers, httpMethod="POST", body = HttpRequestBody.BinaryUpload ctx.request.rawForm)
+        let! result = H.AsyncRequestString(realUri, query=query, headers=headers, httpMethod="POST", body = HttpRequestBody.BinaryUpload ctx.request.rawForm, silentHttpErrors=true)
 
         let resultJson = J.Parse result
 
@@ -86,15 +91,29 @@ let main argv =
             | _ -> false)
           | _ -> failwith "Unexpected response from ASOS API"
           |> Option.get
+
+        DataStore.update variantId item
         
-        return! OK result ctx
+        return! withStatusCode (UTF8.bytes result) ctx.response.status ctx
         }))
 
+  let passThrough =
+    fun (ctx:HttpContext) ->
+      async {
+      let headers = getHeaders ctx
+      let query = getQueryString ctx
+      let realUri = "https://api.asos.com" + ctx.request.path
+      let method = ctx.request.method.ToString()
+
+      let! result = H.AsyncRequestString(realUri, query=query, headers=headers, httpMethod=method, body = HttpRequestBody.BinaryUpload ctx.request.rawForm, silentHttpErrors=true)
+      return! withStatusCode (UTF8.bytes result) ctx.response.status ctx
+      }
 
   let app =
     choose [
-      addToBag
+      //addToBag
       GET >=> path "/hello" >=> Successful.OK "Hello World!"
+      passThrough
       ]
     
   startWebServer defaultConfig app
