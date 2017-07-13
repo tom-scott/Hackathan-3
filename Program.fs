f286efaf3258a94e0cb4d7ede7a80438cdf45e7d
Tom Scott <thomas.scott@asos.com>
2017-07-13 14:04:50 +0100

    Working sample for POST!

--- Program.fs
+++ Program.fs
@@ -2,6 +2,9 @@
 open Suave.Filters
 open Suave.Operators
 open Suave.Successful
+open FSharp.Data
+
+type H = FSharp.Data.Http
 
 [<EntryPoint>]
 let main argv = 
@@ -22,11 +25,56 @@ let main argv =
 
 //pathScan "/commerce/bag/v3/bags/%s/product?expand=total,summary,address,spendLevelDiscount,deliveryoptions&language=en-GB" (fun bagId -> OK (sprintf "THis is bag %s" bag))
 
+  let bagApiUrl = "https://api.asos.com/commerce/bag/v3/bags/"
+
+  let headersNotToPassOn =
+    [|
+      "content-length"
+      "host"
+      "connection"
+    |] |> Set.ofArray
+
   let addToBag =
     POST
-      >=> pathScan "/commerce/bag/v3/bags/%s/product" (fun bagId ->
-        OK (sprintf "THis is bag %s" bagId)
-        )
+      >=> pathScan "/commerce/bag/v3/bags/%s/product" (fun bagId -> (fun ctx ->
+        async {
+        
+        let realUri = sprintf "%s%s/product" bagApiUrl bagId
+        
+        let headers =
+          ctx.request.headers
+          |> Seq.filter (fun (k,_) -> headersNotToPassOn.Contains (k.ToLower()) = false)
+          |> Seq.toList
+
+
+          
+        let body = ctx.request
+        let foo = headers |> Seq.toArray
+
+        let query =
+          ctx.request.query
+          |> Seq.choose (fun (k,v) ->
+            match v with
+            | Some x -> Some (k, x)
+            | None -> None)
+          |> Seq.toList
+
+
+        let! result = H.AsyncRequestString(realUri, query=query, headers=headers, httpMethod="POST", body = HttpRequestBody.BinaryUpload ctx.request.rawForm)
+
+        return! OK result ctx
+        }))
+      //>=> (fun ctx ->
+      //  1
+      //  )
+        
+      //  let realUri = sprintf "%s%s/product" bagApiUrl bagId
+
+
+
+        
+      //  OK (sprintf "THis is bag %s" bagId)
+      //  )
 
   let app =
     choose [
